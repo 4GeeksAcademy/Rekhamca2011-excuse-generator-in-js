091ddc752ea5356248e20bc306ad3e89ec90eb91
Rekhamca2011 <rekhamca2011@gmail.com>
2025-09-02 14:00:59 +0000

    Excuse Generator in JavaScript

---
+++ src/app.js
@@ -0,0 +1,20 @@
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+window.onload = function() {
+  document.querySelector("#heading").innerHTML = 'OMG! you will not believe me but...';
+  document.querySelector("#excuse").innerHTML = random(who) + ' ' + random(action) + ' ' + random(what)+ ' '  + random(when)
+};
+
+function random(arrayWords){
+  let min = 0;
+  let max = arrayWords.length - 1;
+  let randomNumber = getRandomNumber(min, max);
+  return arrayWords[randomNumber];
+}
+
+function getRandomNumber(min,max){
+  return Math.floor(Math.random() * (max-min) + min);
+}
